6af5371188989f164d2a8f6c10cc18510892f64e
Apriana <aprianacahya04@gmail.com>
2024-01-28 00:50:26 +0700

    add filtering dashboard admin

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -3,7 +3,8 @@ import { supabase } from '../../lib/api'
 import formatRupiah from '../../utils/formatRupiah'
 
 export default function Dashboard() {
-  const [date, setDate] = useState(new Date())
+  const [beforeDate, setBeforeDate] = useState('2024-01-30')
+  const [afterDate, setAfterDate] = useState('2024-01-01')
   const [transactionLog, setTransactionLog] = useState([])
   const [orderLog, setOrderLog] = useState([])
   const [loading,setLoading] = useState(true)
@@ -11,24 +12,30 @@ export default function Dashboard() {
 
   useEffect(() => {
     async function getTransactionLog(){
-      const { data } = await supabase.from('transactions').select()
+      const { data } = await supabase.from('transactions').select().gte('created_at', afterDate).lte('created_at', beforeDate)
       setTransactionLog(data)
-      setLoading(false)
     }
     async function getOderLog(){
-      const { data } = await supabase.from('orders').select().eq('oncart', false)
+      const { data } = await supabase.from('orders').select().eq('oncart', false).gte('created_at', afterDate).lte('created_at', beforeDate)
       setOrderLog(data)
+      setLoading(false)
     }
     getTransactionLog()
     getOderLog()
-  },[])
-  console.log(date)
+  },[afterDate, beforeDate])
   if (loading) return <h1>Loading...</h1>
   return (
     <section>
-      <input type="date" name="" id="" onChange={(e)=>setDate(new Date(e.target.value))}/>
+      <label htmlFor="after">
+        <p>After</p>
+        <input type="date" name="after" defaultValue={afterDate} onChange={(e)=>setAfterDate(e.target.value)}/>
+      </label>
+      <label htmlFor="before">
+        <p>Before</p>
+        <input type="date" name="before" defaultValue={beforeDate} onChange={(e)=>setBeforeDate(e.target.value)}/>
+      </label>
       <section>Total Uang yang didapat: {formatRupiah(totalTransaction)}</section>
-      <section>Total Order: {orderLog.length}</section>
+      <section>Total Order: {orderLog?.length}</section>
     </section>
   )
 }
